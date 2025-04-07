690526f709a5b1b3806330dd027e32dd9a846539
Inevitable200tech <abhishek2007tech@gmail.com>
2025-04-07 22:59:17 +0530

    Initial commit

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 import app from './app.js';
 import { startCleanup } from './utils/registries.js';
+import { connectToImageDBs } from './config/db.js';
 
+await connectToImageDBs(); // make sure all connections are set up
 const PORT = 5000;
 
 app.listen(PORT, () => {
