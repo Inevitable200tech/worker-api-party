fff910088c3c15b2058435c791609efa9707546a
Inevitable200tech <abhishek2007tech@gmail.com>
2025-04-08 21:30:45 +0530

    Initial commit

--- config/db.js
+++ config/db.js
@@ -12,14 +12,16 @@ console.log('Loaded IMAGE_DB_URIS:', imageDbURIs);
 console.log('Loaded RECORD_DB_URI:', recordDbURI);
 
 export const connectToImageDBs = async () => {
-  for (const uri of imageDbURIs) {
+  for (const rawUri of imageDbURIs) {
+    const uri = rawUri.trim().replace(/"/g, ''); // remove extra quotes and whitespace
     const conn = await mongoose.createConnection(uri).asPromise();
-    const dbName = new URL(uri).pathname.slice(1); // get DB name from URI
-    conn.name = dbName; // manually assign it
+    const dbName = new URL(uri).pathname.slice(1); // should now properly be "test" or whatever it is
+    conn.name = dbName;
     imageConnections.push(conn);
     console.log(`[INIT] Connected to image DB: ${dbName}`);
   }
 };
+
 
 export const getNextImageDB = () => {
   const conn = imageConnections[roundRobinIndex];
@@ -31,4 +33,4 @@ export const connectToRecordDB = async () => {
   return mongoose.createConnection(recordDbURI).asPromise();
 };
 
-export {imageConnections};
+export { imageConnections };
