b38aed0c9dd54d85711db7453d3fc3430b61504d
Inevitable200tech <abhishek2007tech@gmail.com>
2025-05-09 12:45:53 +0530

    Initial commit

--- routes/imageRoutes.js
+++ routes/imageRoutes.js
@@ -219,7 +219,7 @@ router.post('/upload-zip', upload.single('zip'), async (req, res) => {
 
   const bucket = new GridFSBucket(dbConn.db, {
     bucketName: 'zips',
-    chunkSizeBytes: 25 * 1024 * 1024
+    chunkSizeBytes: 150 * 1024 * 1024
   });
   console.log('[ZIP-UPLOAD] GridFSBucket initialized with chunkSizeBytes =', 25 * 1024 * 1024);
 
