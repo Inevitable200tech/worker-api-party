21b88fb88cec1ab382867e3eb5c9bd826ebc5736
Inevitable200tech <abhishek2007tech@gmail.com>
2025-05-12 20:59:18 +0530

    Initial commit

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 const MAX_DB_CAPACITY_MB = 512; // Each database max capacity
 
